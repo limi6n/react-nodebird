fb7e1d384a384d5b08c72cd1e4355407e508b8e9
limi6n <6nlimitless@gmail.com>
2022-04-18 22:27:37 +0900

    반응형 그리드

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,14 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
+import {Menu, Input, Row, Col} from 'antd';
 
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <Link href="/"><a>노드버드</a></Link>
-      <Link href="/profile"><a>프로필</a></Link>
-      <Link href="/signup"><a>회원가입</a></Link>
-      {children}
+      <Menu mode="horizontal">
+        <Menu.Item>
+          <Link href="/"><a>노드버드</a></Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/profile"><a>프로필</a></Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Input.Search enterButton style={{ verticalAlign: 'middle' }} />
+        </Menu.Item>
+        <Menu.Item>
+          <Link href="/signup"><a>회원가입</a></Link>
+        </Menu.Item>
+      </Menu>
+      <Row gutter={8}>
+        <Col xs={24} md={6}>왼쪽메뉴 </Col>
+        <Col xs={24} md={12}>{children}</Col>
+        <Col xs={24} md={6}>
+          <a href="https://bit.ly/3uQiCrS" target="_blank" rel="noreferrer noopener">Mady by limi</a>
+        </Col>
+      </Row>
+      
     </div>
   );
 };
