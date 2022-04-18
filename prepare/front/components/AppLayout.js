fdc3a9b788ef5e3f8e08be669811eb1ac7867286
limi6n <6nlimitless@gmail.com>
2022-04-18 22:44:00 +0900

    로그인폼

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
 import {Menu, Input, Row, Col} from 'antd';
 
+import UserProfile from "../components/UserProfile";
+import LoginForm from "../components/LoginForm";
+
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
       <Menu mode="horizontal">
@@ -21,7 +26,9 @@ const AppLayout = ({ children }) => {
         </Menu.Item>
       </Menu>
       <Row gutter={8}>
-        <Col xs={24} md={6}>왼쪽메뉴 </Col>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? <UserProfile /> : <LoginForm />}
+        </Col>
         <Col xs={24} md={12}>{children}</Col>
         <Col xs={24} md={6}>
           <a href="https://bit.ly/3uQiCrS" target="_blank" rel="noreferrer noopener">Mady by limi</a>
---
+++ prepare/front/components/UserProfile.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UserProfile = () => {
+    return <div>내 프로필</div>;
+}
+
+export default UserProfile;
