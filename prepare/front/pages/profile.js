e76c585f6dd4f48f825d9aa7b811c59852c16a96
limi6n <6nlimitless@gmail.com>
2022-04-19 15:13:11 +0900

    profile page

--- prepare/front/pages/profile.js
+++ prepare/front/pages/profile.js
@@ -1,14 +1,25 @@
 import React from "react";
 import Head from 'next/head';
 
+import AppLayout from '../components/AppLayout';
+import NicknameEditForm from "../components/NicknameEditForm";
+import FollowList from "../components/FollowList";
+
 const Profile = () => {
+  const followerList = [{nickname:'라미'}, {nickname:'함지'}, {nickname:'예또리'}];
+  const followingList = [{nickname:'라미'}, {nickname:'함지'}, {nickname:'예또리'}];
+
   return (
     <>
       <Head>
           <meta charSet="utf-8" />
           <title>프로필 | NodeBird</title>
       </Head>
-      <div>내 프로필</div>
+      <AppLayout>
+        <NicknameEditForm />
+        <FollowList header="팔로잉 목록" data={followingList} />
+        <FollowList header="팔로워 목록" data={followerList} />
+      </AppLayout>
     </>
   );
 };
